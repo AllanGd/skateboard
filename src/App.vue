d5c62968bfcb17048f719098b097721dda615d86
AllanGd <allan_gd@hotmail.com>
2023-03-26 23:29:50 -0300

    barra de menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import Dashboard from './components/layout/Dashboard.vue';
+
 </script>
 
 <template>
-  
+  <Dashboard />
 </template>
 
 <style scoped>
---
+++ src/components/layout/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-gray-800 text-white max-h-screen h-screen flex">
+        <div class="w-32">
+            <span class="font-semibold px-6 py-3 block">Skateboard</span>
+            <nav class="p-6">
+                <h3 class="opacity-50 uppercase font-semibold tracking-wider mb-6 text-xs">Menu</h3>
+                <div class="space-y-6">
+                    <div v-for="n of 10" class="flex items-center space-x-2">
+                        <div class="bg-orange-500 p-1 rounded-lg">
+                            <HomeIcon />
+                        </div>
+                        <span>Discover</span>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="border-2 border-red-500 flex-1">Content
+            
+        </div>
+        </div>
+</template>
+
+<script lang="ts" setup>
+import HomeIcon from '../icons/HomeIcon.vue';
+
+
+</script>
